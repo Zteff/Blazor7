4cd55b33fb464391d407c4cfea105a2d6ec3f12a
Steffen <dk.steffen@gmail.com>
2022-11-15 13:21:04 +0100

    Initial commit

---
+++ QuickGridDemo.Data/ProductRepository.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using QuickGridDemo.Data.Models;
+
+namespace QuickGridDemo.Data
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly NorthwindContext _context;
+        private IOrderedQueryable<Product>? products;
+
+        public ProductRepository(NorthwindContext context)
+        {
+            _context = context;
+        }
+
+        
+        public IQueryable<Product> SearchProducts(string? filter)
+        {
+            products = _context.Products.AsNoTracking().OrderBy(x => x.ProductId); //.Take(50);
+            if (!string.IsNullOrEmpty(filter))
+            {
+                products = products.Where(x => EF.Functions.Like(x.ProductName, filter.Replace("%", ""))).OrderBy(x => x.ProductId);
+            }
+            return products;
+        }
+    }
+
+    public interface IProductRepository
+    {
+        IQueryable<Product> SearchProducts(string? filter);
+    }
+}
